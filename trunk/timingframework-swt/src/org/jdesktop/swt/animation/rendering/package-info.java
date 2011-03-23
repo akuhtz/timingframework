41a723910c5a5533905a4dbb261d5f15fbc5dac1
hallorant <hallorant@3b6d4fe7-4653-6685-b3f4-cd575a8fdea3>
2011-03-23 03:11:59 +0000

    Fixed Javadoc for the 2.0 release.

--- trunk/timingframework-swt/src/org/jdesktop/swt/animation/rendering/package-info.java
+++ trunk/timingframework-swt/src/org/jdesktop/swt/animation/rendering/package-info.java
@@ -1,5 +1,5 @@
 /**
  * A flexible implementation of rendering on a SWT Canvas component that supports
- * passive rendering.
+ * passive rendering. Active rendering is not possible in SWT.
  */
 package org.jdesktop.swt.animation.rendering;
---
+++ trunk/timingframework-swt/src/org/jdesktop/swt/animation/timing/triggers/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * A simple mechanism for starting animations when specific SWT events occur.
+ */
+package org.jdesktop.swt.animation.timing.triggers;
