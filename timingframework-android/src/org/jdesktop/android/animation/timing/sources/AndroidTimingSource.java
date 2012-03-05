fccc6f57dddfe51f0f0e4160434088eaf57a65a0
hallorant <tim.halloran@surelogic.com>
2012-03-05 18:40:48 +0000

    Added correction for drift of the Android timer.

--- timingframework-android/src/org/jdesktop/android/animation/timing/sources/AndroidTimingSource.java
+++ timingframework-android/src/org/jdesktop/android/animation/timing/sources/AndroidTimingSource.java
@@ -15,44 +15,88 @@ import com.surelogic.Vouch;
 @ThreadSafe
 public final class AndroidTimingSource extends TimingSource {
 
-  private final int f_periodMillis;
+  /**
+   * Flags that no ideal time for the next callback to the periodic task
+   * {@link #f_periodic} declared below has been setup. I.e., the task just
+   * started running.
+   */
+  static private final long NONE = -1;
+
+  private final long f_periodNanos;
   private final AtomicBoolean f_running = new AtomicBoolean(true);
   @Vouch("ThreadSafe")
   private final Activity f_activity;
 
-  private class Periodic implements Runnable {
+  /**
+   * Handler for the periodic task {@link #f_periodic} declared below.
+   * <p>
+   * This state is thread confined to the Android UI thread.
+   */
+  @Vouch("ThreadSafe")
+  private Handler f_handler;
 
-    Periodic(Handler handler) {
-      f_handler = handler;
-    }
+  @Vouch("ThreadSafe")
+  private final Runnable f_periodic = new Runnable() {
 
-    private final Handler f_handler;
+    /**
+     * This represents a relative {@link System#nanoTime()} at which the next
+     * callback in the future to this periodic task should occur.
+     * <p>
+     * This state is thread confined to the Android UI thread.
+     */
+    private long f_ideaNextTickNanoTime = NONE;
 
     public void run() {
       if (f_running.get()) {
         getNotifyTickListenersTask().run();
-        f_handler.postDelayed(new Periodic(f_handler), f_periodMillis);
+        final long now = System.nanoTime();
+        final long delayNanos;
+        if (f_ideaNextTickNanoTime != NONE) {
+          final long delayUntilNext = f_ideaNextTickNanoTime - now;
+          if (delayUntilNext > 0)
+            delayNanos = delayUntilNext;
+          else
+            delayNanos = 0;
+          f_ideaNextTickNanoTime += f_periodNanos;
+        } else {
+          delayNanos = f_periodNanos;
+          f_ideaNextTickNanoTime = now + f_periodNanos;
+        }
+        long delayMillis = TimeUnit.NANOSECONDS.toMillis(delayNanos);
+        if (delayMillis > 0) {
+          f_handler.postDelayed(f_periodic, delayMillis);
+        } else {
+          f_handler.post(f_periodic);
+        }
       }
     }
-  }
+  };
 
+  /**
+   * If the period passed is less than 1 millisecond it will be increased to 1
+   * millisecond.
+   * 
+   * @param period
+   * @param unit
+   * @param activity
+   */
   public AndroidTimingSource(long period, TimeUnit unit, final Activity activity) {
-    int periodMillis = (int) unit.toMillis(period);
-    if (periodMillis < 1)
-      periodMillis = 1;
-    f_periodMillis = periodMillis;
+    final long one = TimeUnit.MILLISECONDS.toNanos(1);
+    long periodNanos = unit.toNanos(period);
+    if (periodNanos < one)
+      periodNanos = one;
+    f_periodNanos = periodNanos;
     if (activity == null)
       throw new IllegalArgumentException(I18N.err(1, "activity"));
     f_activity = activity;
-
   }
 
   @Override
   public void init() {
     f_activity.runOnUiThread(new Runnable() {
       public void run() {
-        final Handler handler = new Handler();
-        handler.postDelayed(new Periodic(handler), f_periodMillis);
+        f_handler = new Handler();
+        f_handler.post(f_periodic);
       }
     });
   }
