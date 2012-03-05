5edfac94191d93c3f7972c542902c3cda95ca78b
hallorant <tim.halloran@surelogic.com>
2012-03-05 19:30:57 +0000

    Added drift correction to the Android timing source.

--- timingframework-android/src/org/jdesktop/android/animation/timing/sources/AndroidTimingSource.java
+++ timingframework-android/src/org/jdesktop/android/animation/timing/sources/AndroidTimingSource.java
@@ -12,6 +12,40 @@ import android.os.Handler;
 import com.surelogic.ThreadSafe;
 import com.surelogic.Vouch;
 
+/**
+ * A timing source based upon the Android
+ * {@link Activity#runOnUiThread(Runnable)} and
+ * {@link Handler#postDelayed(Runnable, long)} methods. Drift correction is
+ * performed by this implementation. This implementation ensures that calls to
+ * registered {@code TickListener} and {@code PostTickListener} objects are
+ * always made within the thread context of the Android UI thread for the passed
+ * {@link Activity}.
+ * <p>
+ * A typical use, where {@code tl} is a {@code TickListener} object, would be
+ * 
+ * <pre>
+ * Activity activity = ...; // Often "this"
+ * TimingSource ts = new AndroidTimingSource(15, TimeUnit.MILLISECONDS, activity);
+ * ts.init(); // starts the timer
+ * 
+ * ts.addTickListener(tl); // tl gets tick notifications in the Android UI thread
+ * 
+ * ts.removeTickListener(tl); // tl stops getting notifications
+ * 
+ * ts.dispose(); // done using ts
+ * </pre>
+ * 
+ * If you are not sure what period to set, use the
+ * {@link #AndroidTimingSource(Activity)} constructor which uses a reasonable
+ * default value of 15 milliseconds.
+ * <p>
+ * Calls to registered {@code TickListener} and {@code PostTickListener} objects
+ * from this timing source are always made in the context of the Android UI
+ * thread. Further, any tasks submitted to {@link #submit(Runnable)} are run in
+ * the thread context of the Android UI thread as well.
+ * 
+ * @author Tim Halloran
+ */
 @ThreadSafe
 public final class AndroidTimingSource extends TimingSource {
 
@@ -73,12 +107,23 @@ public final class AndroidTimingSource extends TimingSource {
   };
 
   /**
-   * If the period passed is less than 1 millisecond it will be increased to 1
-   * millisecond.
+   * Constructs a new instance. The {@link #init()} must be called on the new
+   * instance to start the timer. The {@link #dispose()} method should be called
+   * to stop the timer.
+   * <p>
+   * The Android timer requires a period of at least 1 millisecond. If the
+   * period passed is smaller, it is set to 1 millisecond.
    * 
    * @param period
+   *          the period of time between "tick" events.
    * @param unit
+   *          the time unit of period parameter.
    * @param activity
+   *          the {@link Activity} that manages the connection between Android
+   *          timing and the underlying operating system.
+   * 
+   * @throws IllegalArgumentException
+   *           if <tt>activity<tt> is {@code null}.
    */
   public AndroidTimingSource(long period, TimeUnit unit, final Activity activity) {
     final long one = TimeUnit.MILLISECONDS.toNanos(1);
@@ -89,6 +134,15 @@ public final class AndroidTimingSource extends TimingSource {
     if (activity == null)
       throw new IllegalArgumentException(I18N.err(1, "activity"));
     f_activity = activity;
+  }
+
+  /**
+   * Constructs a new instance with a period of 15 milliseconds. The
+   * {@link #init()} must be called on the new instance to start the timer. The
+   * {@link #dispose()} method should be called to stop the timer.
+   */
+  public AndroidTimingSource(final Activity activity) {
+    this(15, TimeUnit.MILLISECONDS, activity);
   }
 
   @Override
